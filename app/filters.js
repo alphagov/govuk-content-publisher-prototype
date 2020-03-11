073682080a92a134760721810478399a18da1898
Simon Whatley <simonwhatley@gmail.com>
2020-03-11 14:14:26 +0000

    Update filters.js

--- app/filters.js
+++ app/filters.js
@@ -356,6 +356,8 @@ module.exports = function (env) {
       9780596520687
       ISBN-10 0-596-52068-9
       0-596-52068-9
+
+    From: https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s13.html
   ------------------------------------------------------------------ */
   filters.isValidISBN = function(str) {
     var pattern = new RegExp('/^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$/','i'); // fragment locator
